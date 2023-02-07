82d87126e37e1893fd452b0323996940db7391a3
Devinllerena23 <devinllerena12@gmail.com>
2023-02-07 12:25:44 -0500

    menu

---
+++ src/components/Saludos.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Saludos() {
+    return (
+        <div>
+            <h1>Saludos</h1>
+            <strong>
+            Estamos muy felices de tenerte, ¡felicitaciones y bienvenido! ¡Bienvenido a tu nuevo equipo de trabajo!
+             Estamos muy felices de que formes parte de la compañía, esperamos que nuestro vínculo resulte mutuamente beneficioso.
+            </strong>
+        </div>
+    )
+}
